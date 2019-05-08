85296c6d87858e473bab81c8d99adfce20194839
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-05-08 19:30:27 +0900

    Add TerrainAutotile

---
+++ Tests/Tiles/TerrainAutotileTests.cs
@@ -0,0 +1,76 @@
+using NSubstitute;
+
+using NUnit.Framework;
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+using UnityScriptLab.Tiles;
+
+namespace Tests.Tiles {
+  public class TerrainAutotilesTest {
+    ITilemap tilemap;
+    TerrainAutotile tile;
+    TileData result;
+
+    [SetUp]
+    public void Prepare() {
+      tilemap = Substitute.For<ITilemap>();
+      tile = ScriptableObject.CreateInstance<TerrainAutotile>();
+      tile.baseTexture = BuildFakeTexture();
+      result = new TileData();
+    }
+
+    Color[,] texture = new Color[4, 6];
+
+    Texture2D BuildFakeTexture() {
+      Texture2D result = new Texture2D(4, 6);
+      for (int x = 0; x < 4; x++) {
+        for (int y = 0; y < 6; y++) {
+          result.SetPixel(x, y, new Color(x * 0.1f, y * 0.1f, 0));
+          texture[x, y] = result.GetPixel(x, y);
+        }
+      }
+      return result;
+    }
+
+    void AssertTile(params(int, int) [] expectedPixels) {
+      tile.GetTileData(Vector3Int.zero, tilemap, ref result);
+      Color[] pixels = result.sprite.texture.GetPixels(0, 0, 2, 2);
+      for (int i = 0; i < 4; i++) {
+        (int x, int y) = expectedPixels[i];
+        Assert.That(pixels[i] == texture[x, y], $"But pixel {i} was {pixels[i]} ({texture[x, y]})");
+      }
+    }
+
+    [Test]
+    public void NoNeighborsTest() {
+      tile.GetTileData(Vector3Int.zero, tilemap, ref result);
+      AssertTile((0, 4), (1, 4), (0, 5), (1, 5));
+    }
+
+    [Test]
+    public void OneSideTest() {
+      tilemap.GetTile(Vector3Int.up).Returns(tile);
+      tile.GetTileData(Vector3Int.zero, tilemap, ref result);
+      AssertTile((0, 4), (1, 4), (0, 1), (3, 1));
+    }
+
+    [Test]
+    public void TwoSidesCornerTest() {
+      tilemap.GetTile(Vector3Int.down).Returns(tile);
+      tilemap.GetTile(Vector3Int.right).Returns(tile);
+      tile.GetTileData(Vector3Int.zero, tilemap, ref result);
+      AssertTile((0, 2), (3, 4), (0, 3), (1, 3));
+    }
+
+    [Test]
+    public void ThreeSidesCornerTest() {
+      tilemap.GetTile(Vector3Int.left).Returns(tile);
+      tilemap.GetTile(Vector3Int.right).Returns(tile);
+      tilemap.GetTile(Vector3Int.up).Returns(tile);
+      tile.GetTileData(Vector3Int.zero, tilemap, ref result);
+      AssertTile((2, 0), (1, 0), (2, 5), (3, 5));
+    }
+  }
+}
---
+++ UnityScriptLab/Tiles/TerrainAutotile.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace UnityScriptLab.Tiles {
+  [Serializable]
+  [CreateAssetMenu(fileName = "New Terrain Auto Tile", menuName = "Tiles/Terrain Auto Tile")]
+  public class TerrainAutotile : TileBase {
+    public Tile.ColliderType colliderType;
+    public Texture2D baseTexture;
+
+    public override void RefreshTile(Vector3Int location, ITilemap tileMap) {
+      for (int y = -1; y <= 1; y++)
+        for (int x = -1; x <= 1; x++) {
+          Vector3Int position = new Vector3Int(location.x + x, location.y + y, location.z);
+          TileBase t = tileMap.GetTile(position);
+          if (t == this) {
+            tileMap.RefreshTile(position);
+          }
+        }
+    }
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData) {
+      tileData.colliderType = colliderType;
+      tileData.color = Color.white;
+      tileData.transform = Matrix4x4.identity;
+      Neighborhood neighborhood = new Neighborhood(this, tilemap, position);
+      TileParts tileParts = GetTileParts(neighborhood);
+      tileData.sprite = BuildSprite(tileParts);
+    }
+
+    enum TilePart {
+      BLL,
+      BL,
+      BR,
+      BRR,
+      LB,
+      CBL,
+      CBR,
+      RB,
+      LT,
+      CTL,
+      CTR,
+      RT,
+      TLL,
+      TL,
+      TR,
+      TRR,
+      Single_BL,
+      Single_BR,
+      Corner_BL,
+      Corner_BR,
+      Single_TL,
+      Single_TR,
+      Corner_TL,
+      Corner_TR,
+    }
+
+    TilePart TopLeft(Neighborhood neighborhood) {
+      if (neighborhood.HasTop() && neighborhood.HasLeft()) {
+        return neighborhood.HasTopLeft() ? TilePart.CBR : TilePart.Corner_TL;
+      }
+      if (neighborhood.HasTop() && !neighborhood.HasLeft()) {
+        return TilePart.LB;
+      }
+      if (!neighborhood.HasTop() && neighborhood.HasLeft()) {
+        return TilePart.TR;
+      }
+      if (!neighborhood.HasTop() && !neighborhood.HasLeft() && neighborhood.HasRight() && neighborhood.HasBottom()) {
+        return TilePart.TLL;
+      }
+      return TilePart.Single_TL;
+    }
+
+    TilePart TopRight(Neighborhood neighborhood) {
+      if (neighborhood.HasTop() && neighborhood.HasRight()) {
+        return neighborhood.HasTopRight() ? TilePart.CBL : TilePart.Corner_TR;
+      }
+      if (neighborhood.HasTop() && !neighborhood.HasRight()) {
+        return TilePart.RB;
+      }
+      if (!neighborhood.HasTop() && neighborhood.HasRight()) {
+        return TilePart.TL;
+      }
+      if (!neighborhood.HasTop() && !neighborhood.HasRight() && neighborhood.HasLeft() && neighborhood.HasBottom()) {
+        return TilePart.TRR;
+      }
+      return TilePart.Single_TR;
+    }
+
+    TilePart BottomLeft(Neighborhood neighborhood) {
+      if (neighborhood.HasBottom() && neighborhood.HasLeft()) {
+        return neighborhood.HasBottomLeft() ? TilePart.CTR : TilePart.Corner_BL;
+      }
+      if (neighborhood.HasBottom() && !neighborhood.HasLeft()) {
+        return TilePart.LT;
+      }
+      if (!neighborhood.HasBottom() && neighborhood.HasLeft()) {
+        return TilePart.BR;
+      }
+      if (!neighborhood.HasBottom() && !neighborhood.HasLeft() && neighborhood.HasRight() && neighborhood.HasBottom()) {
+        return TilePart.BLL;
+      }
+      return TilePart.Single_BL;
+    }
+
+    TilePart BottomRight(Neighborhood neighborhood) {
+      if (neighborhood.HasBottom() && neighborhood.HasRight()) {
+        return neighborhood.HasBottomRight() ? TilePart.CTL : TilePart.Corner_BR;
+      }
+      if (neighborhood.HasBottom() && !neighborhood.HasRight()) {
+        return TilePart.RT;
+      }
+      if (!neighborhood.HasBottom() && neighborhood.HasRight()) {
+        return TilePart.BL;
+      }
+      if (!neighborhood.HasBottom() && !neighborhood.HasRight() && neighborhood.HasLeft() && neighborhood.HasBottom()) {
+        return TilePart.BRR;
+      }
+      return TilePart.Single_BR;
+    }
+    struct TileParts {
+      public TilePart topLeft;
+      public TilePart topRight;
+      public TilePart bottomLeft;
+      public TilePart bottomRight;
+
+      public TileParts(TilePart bottomLeft, TilePart bottomRight, TilePart topLeft, TilePart topRight) {
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+      }
+
+      public override string ToString() {
+        return $"TileParts({topLeft}, {topRight}, {bottomLeft}, {bottomRight})";
+      }
+    }
+
+    TileParts GetTileParts(Neighborhood neighborhood) {
+      return new TileParts(BottomLeft(neighborhood), BottomRight(neighborhood), TopLeft(neighborhood), TopRight(neighborhood));
+    }
+
+    Dictionary<TileParts, Sprite> sprites = new Dictionary<TileParts, Sprite>();
+
+    Sprite BuildSprite(TileParts tileParts) {
+      if (sprites.ContainsKey(tileParts)) {
+        return sprites[tileParts];
+      }
+      (int width, int height) = PartDimensions;
+      Texture2D target = new Texture2D(width * 2, height * 2);
+
+      int x;
+      int y;
+
+      (x, y) = TilePartPosition(tileParts.bottomLeft);
+      target.SetPixels(0, 0, width, height, baseTexture.GetPixels(x, y, width, height));
+      (x, y) = TilePartPosition(tileParts.bottomRight);
+      target.SetPixels(width, 0, width, height, baseTexture.GetPixels(x, y, width, height));
+      (x, y) = TilePartPosition(tileParts.topLeft);
+      target.SetPixels(0, height, width, height, baseTexture.GetPixels(x, y, width, height));
+      (x, y) = TilePartPosition(tileParts.topRight);
+      target.SetPixels(width, height, width, height, baseTexture.GetPixels(x, y, width, height));
+      target.Apply();
+
+      Sprite result = Sprite.Create(target, new Rect(0, 0, width * 2, height * 2), new Vector2(0.5f, 0.5f), width * 2);
+      sprites[tileParts] = result;
+      return result;
+    }
+
+    (int, int) PartDimensions => (baseTexture.width / 4, baseTexture.height / 6);
+
+    (int, int) TilePartPosition(TilePart part) {
+      (int width, int height) = PartDimensions;
+      return (width * ((int) part % 4), height * ((int) part / 4));
+    }
+  }
+}
