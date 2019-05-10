a8dd43dd40c794066f0784b14b5e536c35cc31d1
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-05-10 20:07:22 +0900

    Reorder functions and internal classes

--- UnityScriptLab/Tiles/TerrainAutotile.cs
+++ UnityScriptLab/Tiles/TerrainAutotile.cs
@@ -7,11 +7,14 @@ using UnityEngine.Tilemaps;
 
 namespace UnityScriptLab.Tiles {
   using D = Direction;
+
   [Serializable]
   [CreateAssetMenu(fileName = "New Terrain Auto Tile", menuName = "Tiles/Terrain Auto Tile")]
   public class TerrainAutotile : TileBase {
     public Tile.ColliderType colliderType = Tile.ColliderType.Sprite;
     public Texture2D baseTexture;
+
+    Dictionary<TileParts, Sprite> sprites = new Dictionary<TileParts, Sprite>();
 
     public override void RefreshTile(Vector3Int location, ITilemap tileMap) {
       for (int y = -1; y <= 1; y++)
@@ -35,6 +38,89 @@ namespace UnityScriptLab.Tiles {
         sprites[tileParts] = BuildSprite(tileParts);
       }
       tileData.sprite = sprites[tileParts];
+    }
+
+    struct TileParts {
+      public TilePart topLeft;
+      public TilePart topRight;
+      public TilePart bottomLeft;
+      public TilePart bottomRight;
+
+      public static TileParts Construct(Neighborhood neighborhood) {
+        return new TileParts(
+          TopLeft(neighborhood),
+          TopLeft(neighborhood, flipHorizontal : true),
+          TopLeft(neighborhood, flipVertical : true),
+          TopLeft(neighborhood, flipHorizontal : true, flipVertical : true)
+        );
+      }
+
+      public override string ToString() {
+        return $"TileParts({topLeft}, {topRight}, {bottomLeft}, {bottomRight})";
+      }
+
+      TileParts(TilePart topLeft, TilePart topRight, TilePart bottomLeft, TilePart bottomRight) {
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+      }
+
+      static TilePart TopLeft(Neighborhood neighborhood, bool flipHorizontal = false, bool flipVertical = false) {
+        Func<D, D> applyFlip = d => d.Flip(flipHorizontal, flipVertical);
+        D up = applyFlip(D.Up);
+        D left = applyFlip(D.Left);
+        D upLeft = applyFlip(D.UpLeft);
+        D downRight = applyFlip(D.DownRight);
+        D down = applyFlip(D.Down);
+        D right = applyFlip(D.Right);
+
+        if (neighborhood.Has(up) && neighborhood.Has(left)) {
+          if (neighborhood.Has(upLeft)) {
+            return new AreaCenter(downRight);
+          } else {
+            return new ConvexCorner(upLeft);
+          }
+        }
+        if (neighborhood.Has(up) && !neighborhood.Has(left)) {
+          return new Edge(left, down);
+        }
+        if (!neighborhood.Has(up) && neighborhood.Has(left)) {
+          return new Edge(up, right);
+        }
+        if (!neighborhood.Has(up) && !neighborhood.Has(left) && neighborhood.Has(right) && neighborhood.Has(down)) {
+          return new Corner(upLeft);
+        }
+        return new SingleTileCorner(upLeft);
+      }
+    }
+
+    Sprite BuildSprite(TileParts tileParts) {
+      (int width, int height) = PartDimensions;
+      Texture2D target = new Texture2D(width * 2, height * 2);
+
+      int x;
+      int y;
+
+      (x, y) = TilePartPosition(tileParts.bottomLeft);
+      target.SetPixels(0, 0, width, height, baseTexture.GetPixels(x, y, width, height));
+      (x, y) = TilePartPosition(tileParts.bottomRight);
+      target.SetPixels(width, 0, width, height, baseTexture.GetPixels(x, y, width, height));
+      (x, y) = TilePartPosition(tileParts.topLeft);
+      target.SetPixels(0, height, width, height, baseTexture.GetPixels(x, y, width, height));
+      (x, y) = TilePartPosition(tileParts.topRight);
+      target.SetPixels(width, height, width, height, baseTexture.GetPixels(x, y, width, height));
+      target.Apply();
+
+      return Sprite.Create(target, new Rect(0, 0, width * 2, height * 2), new Vector2(0.5f, 0.5f), width * 2);
+    }
+
+    (int, int) PartDimensions => (baseTexture.width / 4, baseTexture.height / 6);
+
+    (int, int) TilePartPosition(TilePart part) {
+      (int width, int height) = PartDimensions;
+      (int x, int y) = part.Position;
+      return (width * x, height * y);
     }
 
     interface TilePart {
@@ -96,10 +182,10 @@ namespace UnityScriptLab.Tiles {
         return new Edge(main.Flip(horizontal, vertical), secondary.Flip(horizontal, vertical));
       }
 
-      public (int, int) Position {
+      public(int, int) Position {
         get {
           if (main == D.Up || main == D.Down) {
-            return (secondary == D.Left ? 1: 2, main == D.Up ? 3 : 0);
+            return (secondary == D.Left ? 1 : 2, main == D.Up ? 3 : 0);
           } else {
             return (main == D.Left ? 0 : 3, secondary == D.Up ? 2 : 1);
           }
@@ -112,91 +198,6 @@ namespace UnityScriptLab.Tiles {
       }
 
       public override int GetHashCode() => $"{GetType().Name}-{main}-{secondary}".GetHashCode();
-    }
-
-    static TilePart TopLeft(Neighborhood neighborhood, bool flipHorizontal = false, bool flipVertical = false) {
-      Func<D, D> applyFlip = d => d.Flip(flipHorizontal, flipVertical);
-      D up = applyFlip(D.Up);
-      D left = applyFlip(D.Left);
-      D upLeft = applyFlip(D.UpLeft);
-      D downRight = applyFlip(D.DownRight);
-      D down = applyFlip(D.Down);
-      D right = applyFlip(D.Right);
-
-      if (neighborhood.Has(up) && neighborhood.Has(left)) {
-        if (neighborhood.Has(upLeft)) {
-          return new AreaCenter(downRight);
-        } else {
-          return new ConvexCorner(upLeft);
-        }
-      }
-      if (neighborhood.Has(up) && !neighborhood.Has(left)) {
-        return new Edge(left, down);
-      }
-      if (!neighborhood.Has(up) && neighborhood.Has(left)) {
-        return new Edge(up, right);
-      }
-      if (!neighborhood.Has(up) && !neighborhood.Has(left) && neighborhood.Has(right) && neighborhood.Has(down)) {
-        return new Corner(upLeft);
-      }
-      return new SingleTileCorner(upLeft);
-    }
-
-    struct TileParts {
-      public TilePart topLeft;
-      public TilePart topRight;
-      public TilePart bottomLeft;
-      public TilePart bottomRight;
-
-      public static TileParts Construct(Neighborhood neighborhood) {
-        return new TileParts(
-          TopLeft(neighborhood),
-          TopLeft(neighborhood, flipHorizontal: true),
-          TopLeft(neighborhood, flipVertical: true),
-          TopLeft(neighborhood, flipHorizontal: true, flipVertical: true)
-        );
-      }
-
-      TileParts(TilePart topLeft, TilePart topRight, TilePart bottomLeft, TilePart bottomRight) {
-        this.topLeft = topLeft;
-        this.topRight = topRight;
-        this.bottomLeft = bottomLeft;
-        this.bottomRight = bottomRight;
-      }
-
-      public override string ToString() {
-        return $"TileParts({topLeft}, {topRight}, {bottomLeft}, {bottomRight})";
-      }
-    }
-
-    Dictionary<TileParts, Sprite> sprites = new Dictionary<TileParts, Sprite>();
-
-    Sprite BuildSprite(TileParts tileParts) {
-      (int width, int height) = PartDimensions;
-      Texture2D target = new Texture2D(width * 2, height * 2);
-
-      int x;
-      int y;
-
-      (x, y) = TilePartPosition(tileParts.bottomLeft);
-      target.SetPixels(0, 0, width, height, baseTexture.GetPixels(x, y, width, height));
-      (x, y) = TilePartPosition(tileParts.bottomRight);
-      target.SetPixels(width, 0, width, height, baseTexture.GetPixels(x, y, width, height));
-      (x, y) = TilePartPosition(tileParts.topLeft);
-      target.SetPixels(0, height, width, height, baseTexture.GetPixels(x, y, width, height));
-      (x, y) = TilePartPosition(tileParts.topRight);
-      target.SetPixels(width, height, width, height, baseTexture.GetPixels(x, y, width, height));
-      target.Apply();
-
-      return Sprite.Create(target, new Rect(0, 0, width * 2, height * 2), new Vector2(0.5f, 0.5f), width * 2);
-    }
-
-    (int, int) PartDimensions => (baseTexture.width / 4, baseTexture.height / 6);
-
-    (int, int) TilePartPosition(TilePart part) {
-      (int width, int height) = PartDimensions;
-      (int x, int y) = part.Position;
-      return (width * x, height * y);
     }
   }
 }
