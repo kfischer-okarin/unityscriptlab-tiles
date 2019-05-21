29d97fd3e1d5cc1a6076641f3efaef0c01b10f0f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-05-21 13:35:10 +0900

    Rewrite Neighborhood to struct

--- UnityScriptLab/Tiles/Util/Neighborhood.cs
+++ UnityScriptLab/Tiles/Util/Neighborhood.cs
@@ -5,17 +5,11 @@ using UnityEngine.Tilemaps;
 
 namespace UnityScriptLab.Tiles.Util {
   using D = Direction;
-  public class Neighborhood {
-    TileBase tile;
-    ITilemap tilemap;
-    Vector3Int position;
+  public struct Neighborhood {
     uint neighborsFlag;
 
     public Neighborhood(TileBase tile, ITilemap tilemap, Vector3Int position) {
-      this.tile = tile;
-      this.tilemap = tilemap;
-      this.position = position;
-      this.neighborsFlag = CalcNeighborsFlag();
+      this.neighborsFlag = FlagCalculator.Calc(tile, tilemap, position);
     }
 
     public bool Has(Direction direction) {
@@ -41,36 +35,54 @@ namespace UnityScriptLab.Tiles.Util {
       }
     }
 
-    uint CalcNeighborsFlag() {
-      uint result = 0;
-      uint flag = 1;
-      for (int y = 1; y >= -1; y--) {
-        for (int x = 1; x >= -1; x--) {
-          if (x == 0 && y == 0) {
-            continue;
-          }
-
-          Vector3Int neighborPos = new Vector3Int(position.x + x, position.y + y, position.z);
-          if (HasTile(neighborPos)) {
-            result |= flag;
-          }
-          flag <<= 1;
-        }
-      }
-      return result;
-    }
-
-    bool HasTile(Vector3Int pos) {
-      TileBase t = tilemap.GetTile(pos);
-      return t != null && t == tile;
-    }
-
     bool CheckFlag(uint flag) {
       return (neighborsFlag & flag) == flag;
     }
 
     public override string ToString() {
       return $"Neighborhood({Convert.ToString(neighborsFlag, 2)})";
+    }
+
+    class FlagCalculator {
+      TileBase tile;
+      ITilemap tilemap;
+      Vector3Int position;
+
+      public static uint Calc(TileBase tile, ITilemap tilemap, Vector3Int position) {
+        return new FlagCalculator(tile, tilemap, position).NeighborsFlag;
+      }
+
+      public FlagCalculator(TileBase tile, ITilemap tilemap, Vector3Int position) {
+        this.tile = tile;
+        this.tilemap = tilemap;
+        this.position = position;
+      }
+
+      uint NeighborsFlag {
+        get {
+          uint result = 0;
+          uint flag = 1;
+          for (int y = 1; y >= -1; y--) {
+            for (int x = 1; x >= -1; x--) {
+              if (x == 0 && y == 0) {
+                continue;
+              }
+
+              Vector3Int neighborPos = new Vector3Int(position.x + x, position.y + y, position.z);
+              if (HasTile(neighborPos)) {
+                result |= flag;
+              }
+              flag <<= 1;
+            }
+          }
+          return result;
+        }
+      }
+
+      bool HasTile(Vector3Int pos) {
+        TileBase t = tilemap.GetTile(pos);
+        return t != null && t == tile;
+      }
     }
   }
 }
